6846e6991aef039461c06137fe312212324e019b
reddy <ext-vishnuvardhan.reddy@here.com>
2020-05-16 14:50:36 +0530

    Added order_of_operations.py

---
+++ chapter_1/order_of_operations.py
@@ -0,0 +1,24 @@
+
+expr = 2 + 3 * 6
+print("The output of expr : ", expr)
+
+expr_1 = (2 + 3) * 6
+print("The output of expr_1 : ", expr_1)
+
+expr_2 = 2 ** 8
+print("The output of expr_2 : ", expr_2)
+
+expr_3 = 23 / 7
+print("The output of expr_3 : ", expr_3)
+
+expr_4 = 23 // 7
+print("The output of expr_4 : ", expr_4)
+
+expr_5 = 23 % 7
+print("The output of expr_5 : ", expr_5)
+
+expr_6 = 2      +       2
+print("The output of expr_6 : ", expr_6)
+
+expr_7 = (5 - 1) * ((7 + 1) / (3 - 1))
+print("The output of expr_7 : ", expr_7)
